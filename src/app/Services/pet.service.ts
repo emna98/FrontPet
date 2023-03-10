9f47d2813ecf611ab9acdae7b7e1c820db9adaa1
emna98 <emnabelhaj98@gmail.com>
2023-03-10 12:04:28 +0100

    first commit

---
+++ src/app/Services/pet.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+
+const URL_PET=  "http://localhost:8092/api/v1/pets";
+const URL_TREATMENT=  "http://localhost:8092/api/v1/treatments";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetService {
+  constructor(private http: HttpClient) {
+
+  }
+
+  getAllPets(): Observable<any> {
+    return this.http.get<any>(URL_PET)
+  }
+  getAllTreatments(): Observable<any> {
+    return this.http.get<any>(URL_TREATMENT)
+  }
+  getPetById(id:number): Observable<any> {
+    return this.http.get<any>(URL_PET+"/"+id)
+  }
+  
+  getPetTreatmentById(id:number|string|undefined): Observable<any> {
+    return this.http.get<any>(URL_TREATMENT+"/"+id)
+  }
+  savePet(pet:any): Observable<any> {
+    return this.http.post<any>(URL_PET,pet)
+  }
+  saveTreatment(treatment:any): Observable<any> {
+    return this.http.post<any>(URL_TREATMENT,treatment)
+  }
+  updatePetTreatment(pet:any): Observable<any> {
+    return this.http.put<any>(URL_PET,pet)
+  }
+
+  /********************************************************************* */
+  saveOwner(owner:any): Observable<any> {
+    return this.http.post<any>(` http://localhost:3000/owner`,owner)
+  }
+  getPetOwnerById(id:number|string|undefined): Observable<any> {
+    return this.http.get<any>(` http://localhost:3000/owner/${id}`)
+  }
+}
